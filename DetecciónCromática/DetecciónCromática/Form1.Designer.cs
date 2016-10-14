3262ad41f5d4ef7d408f8332222d25d0714b2f2a
Lizbeth <lizabeth.malv@gmail.com>
2016-10-14 09:14:23 -0500

    Deteccion Cromatica

---
+++ DetecciónCromática/DetecciónCromática/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace DetecciónCromática
+{
+    unsafe public partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_RGB = new System.Windows.Forms.Button();
+            this.BTN_YUV = new System.Windows.Forms.Button();
+            this.BTN_HSV = new System.Windows.Forms.Button();
+            this.LBL_STATUSRGB = new System.Windows.Forms.Label();
+            this.LBL_STATUSYUV = new System.Windows.Forms.Label();
+            this.LBL_STATUSHSV = new System.Windows.Forms.Label();
+            this.LBL_200 = new System.Windows.Forms.Label();
+            this.LBL_200YUV = new System.Windows.Forms.Label();
+            this.LBL_200HSV = new System.Windows.Forms.Label();
+            this.LBL_IMAGES = new System.Windows.Forms.Label();
+            this.LBL_IMAGERGB = new System.Windows.Forms.Label();
+            this.LBL_IMAGEYUV = new System.Windows.Forms.Label();
+            this.LBL_IMAGEHSV = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BTN_RGB
+            // 
+            this.BTN_RGB.Location = new System.Drawing.Point(12, 29);
+            this.BTN_RGB.Name = "BTN_RGB";
+            this.BTN_RGB.Size = new System.Drawing.Size(75, 23);
+            this.BTN_RGB.TabIndex = 0;
+            this.BTN_RGB.Text = "RGB";
+            this.BTN_RGB.UseVisualStyleBackColor = true;
+            this.BTN_RGB.Click += new System.EventHandler(this.BTN_RGB_Click);
+            // 
+            // BTN_YUV
+            // 
+            this.BTN_YUV.Location = new System.Drawing.Point(12, 69);
+            this.BTN_YUV.Name = "BTN_YUV";
+            this.BTN_YUV.Size = new System.Drawing.Size(75, 23);
+            this.BTN_YUV.TabIndex = 1;
+            this.BTN_YUV.Text = "YUV";
+            this.BTN_YUV.UseVisualStyleBackColor = true;
+            this.BTN_YUV.Click += new System.EventHandler(this.BTN_YUV_Click);
+            // 
+            // BTN_HSV
+            // 
+            this.BTN_HSV.Location = new System.Drawing.Point(12, 108);
+            this.BTN_HSV.Name = "BTN_HSV";
+            this.BTN_HSV.Size = new System.Drawing.Size(75, 23);
+            this.BTN_HSV.TabIndex = 16;
+            this.BTN_HSV.Text = "HSV";
+            this.BTN_HSV.UseVisualStyleBackColor = true;
+            this.BTN_HSV.Click += new System.EventHandler(this.BTN_HSV_Click);
+            // 
+            // LBL_STATUSRGB
+            // 
+            this.LBL_STATUSRGB.AutoSize = true;
+            this.LBL_STATUSRGB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_STATUSRGB.Location = new System.Drawing.Point(93, 32);
+            this.LBL_STATUSRGB.Name = "LBL_STATUSRGB";
+            this.LBL_STATUSRGB.Size = new System.Drawing.Size(69, 17);
+            this.LBL_STATUSRGB.TabIndex = 17;
+            this.LBL_STATUSRGB.Text = "STATUS";
+            // 
+            // LBL_STATUSYUV
+            // 
+            this.LBL_STATUSYUV.AutoSize = true;
+            this.LBL_STATUSYUV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_STATUSYUV.Location = new System.Drawing.Point(95, 72);
+            this.LBL_STATUSYUV.Name = "LBL_STATUSYUV";
+            this.LBL_STATUSYUV.Size = new System.Drawing.Size(69, 17);
+            this.LBL_STATUSYUV.TabIndex = 18;
+            this.LBL_STATUSYUV.Text = "STATUS";
+            // 
+            // LBL_STATUSHSV
+            // 
+            this.LBL_STATUSHSV.AutoSize = true;
+            this.LBL_STATUSHSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_STATUSHSV.Location = new System.Drawing.Point(95, 111);
+            this.LBL_STATUSHSV.Name = "LBL_STATUSHSV";
+            this.LBL_STATUSHSV.Size = new System.Drawing.Size(69, 17);
+            this.LBL_STATUSHSV.TabIndex = 19;
+            this.LBL_STATUSHSV.Text = "STATUS";
+            // 
+            // LBL_200
+            // 
+            this.LBL_200.AutoSize = true;
+            this.LBL_200.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_200.Location = new System.Drawing.Point(249, 35);
+            this.LBL_200.Name = "LBL_200";
+            this.LBL_200.Size = new System.Drawing.Size(40, 17);
+            this.LBL_200.TabIndex = 20;
+            this.LBL_200.Text = "/200";
+            // 
+            // LBL_200YUV
+            // 
+            this.LBL_200YUV.AutoSize = true;
+            this.LBL_200YUV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_200YUV.Location = new System.Drawing.Point(249, 72);
+            this.LBL_200YUV.Name = "LBL_200YUV";
+            this.LBL_200YUV.Size = new System.Drawing.Size(40, 17);
+            this.LBL_200YUV.TabIndex = 21;
+            this.LBL_200YUV.Text = "/200";
+            // 
+            // LBL_200HSV
+            // 
+            this.LBL_200HSV.AutoSize = true;
+            this.LBL_200HSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_200HSV.Location = new System.Drawing.Point(249, 111);
+            this.LBL_200HSV.Name = "LBL_200HSV";
+            this.LBL_200HSV.Size = new System.Drawing.Size(40, 17);
+            this.LBL_200HSV.TabIndex = 22;
+            this.LBL_200HSV.Text = "/200";
+            // 
+            // LBL_IMAGES
+            // 
+            this.LBL_IMAGES.AutoSize = true;
+            this.LBL_IMAGES.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_IMAGES.Location = new System.Drawing.Point(217, 9);
+            this.LBL_IMAGES.Name = "LBL_IMAGES";
+            this.LBL_IMAGES.Size = new System.Drawing.Size(66, 17);
+            this.LBL_IMAGES.TabIndex = 23;
+            this.LBL_IMAGES.Text = "IMAGES";
+            // 
+            // LBL_IMAGERGB
+            // 
+            this.LBL_IMAGERGB.AutoSize = true;
+            this.LBL_IMAGERGB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_IMAGERGB.Location = new System.Drawing.Point(217, 35);
+            this.LBL_IMAGERGB.Name = "LBL_IMAGERGB";
+            this.LBL_IMAGERGB.Size = new System.Drawing.Size(17, 17);
+            this.LBL_IMAGERGB.TabIndex = 24;
+            this.LBL_IMAGERGB.Text = "0";
+            // 
+            // LBL_IMAGEYUV
+            // 
+            this.LBL_IMAGEYUV.AutoSize = true;
+            this.LBL_IMAGEYUV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_IMAGEYUV.Location = new System.Drawing.Point(217, 72);
+            this.LBL_IMAGEYUV.Name = "LBL_IMAGEYUV";
+            this.LBL_IMAGEYUV.Size = new System.Drawing.Size(17, 17);
+            this.LBL_IMAGEYUV.TabIndex = 25;
+            this.LBL_IMAGEYUV.Text = "0";
+            // 
+            // LBL_IMAGEHSV
+            // 
+            this.LBL_IMAGEHSV.AutoSize = true;
+            this.LBL_IMAGEHSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBL_IMAGEHSV.Location = new System.Drawing.Point(218, 111);
+            this.LBL_IMAGEHSV.Name = "LBL_IMAGEHSV";
+            this.LBL_IMAGEHSV.Size = new System.Drawing.Size(17, 17);
+            this.LBL_IMAGEHSV.TabIndex = 26;
+            this.LBL_IMAGEHSV.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 159);
+            this.Controls.Add(this.LBL_IMAGEHSV);
+            this.Controls.Add(this.LBL_IMAGEYUV);
+            this.Controls.Add(this.LBL_IMAGERGB);
+            this.Controls.Add(this.LBL_IMAGES);
+            this.Controls.Add(this.LBL_200HSV);
+            this.Controls.Add(this.LBL_200YUV);
+            this.Controls.Add(this.LBL_200);
+            this.Controls.Add(this.LBL_STATUSHSV);
+            this.Controls.Add(this.LBL_STATUSYUV);
+            this.Controls.Add(this.LBL_STATUSRGB);
+            this.Controls.Add(this.BTN_HSV);
+            this.Controls.Add(this.BTN_YUV);
+            this.Controls.Add(this.BTN_RGB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_RGB;
+        private System.Windows.Forms.Button BTN_YUV;
+        private System.Windows.Forms.Button BTN_HSV;
+        private System.Windows.Forms.Label LBL_STATUSRGB;
+        private System.Windows.Forms.Label LBL_STATUSYUV;
+        private System.Windows.Forms.Label LBL_STATUSHSV;
+        private System.Windows.Forms.Label LBL_200;
+        private System.Windows.Forms.Label LBL_200YUV;
+        private System.Windows.Forms.Label LBL_200HSV;
+        private System.Windows.Forms.Label LBL_IMAGES;
+        private System.Windows.Forms.Label LBL_IMAGERGB;
+        private System.Windows.Forms.Label LBL_IMAGEYUV;
+        private System.Windows.Forms.Label LBL_IMAGEHSV;
+    }
+}
+
---
+++ DetecciónCromática/DetecciónCromática/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+/**
+
+* Esta clase contiene los métodos relacionados con los eventos que se presentan en la interfaz de usuario.
+
+* @author: Lizbeth Peralta Malváez
+
+* @version: 14/10/2016/C
+
+*/
+
+namespace DetecciónCromática
+{
+    public partial class Form1 : Form
+    {
+        //Campos de la clase
+        int minR, maxR, minG, maxG, minB, maxB;
+        int minH, maxH, minS, maxS, minV2, maxV2;
+
+        string[] pathImages;
+        Bitmap image;
+        Bitmap copiaImagen;
+        Color pixel;
+        //Las imágenes serán guardadas Desktop
+        string filePath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /**BTN_RGB_Click
+        * Al dar click en "RGB", se  llama a la función getRGB
+        */
+        private void BTN_RGB_Click(object sender, EventArgs e)
+        {
+                getRGB();
+        }
+
+        /**BTN_YUV_Click
+       * Al dar click en "YUV", se  llama a la función getYUV
+         * Cuando el método termina, la interfaz pondrá un "DONE"
+       */
+        private void BTN_YUV_Click(object sender, EventArgs e)
+        {
+                getImagesYUV();
+                LBL_STATUSYUV.Text = "DONE";
+        }
+
+        /**BTN_HSV_Click
+       * Al dar click en "HSV", se  llama a la función getHSV
+       */
+        private void BTN_HSV_Click(object sender, EventArgs e)
+        {
+                getHSV();     
+        }
+
+        /**getRGB
+        * Asigna el rango en RGB del color verde y llama al método getImagesRGB.
+         * Cuando el método termina, la interfaz pondrá un "DONE"
+        */
+        public void getRGB()
+        {
+            minR = 0; minG = 35; minB = 0;
+            maxR = 0; maxG = 255; maxB = 0;
+
+            getImagesRGB();
+            LBL_STATUSRGB.Text = "DONE";
+        }
+
+        /**getHSV
+       * Asigna el rango en HSV del color verde y llama al método getImagesHSV.
+        * Cuando el método termina, la interfaz pondrá un "DONE"
+       */
+        public void getHSV() 
+        {
+            minH = 90; maxH = 150;
+
+            minS = 50;
+            maxS = 100;
+
+            minV2 = 15;
+            maxV2 = 100;
+
+            getImagesHSV();
+            LBL_STATUSHSV.Text = "DONE";
+        }
+
+        /**getImagesRGB
+         * Primero se crea una carpeta donde se guardarán las imágenes modificadas.
+         * Se obtendrá de cada imagen su RGB. Si éste está dentro del rango obtenido en el método
+         * getRGB, se pintará de blanco el pixel. Si no está dentro del rango, se quedará con su color
+         * original.
+         * Cuando se termine de modificar cada imagen, se sumará el parámetro number, el cual es 
+         * utilizado en la interfaz para indicar al usuario cuánto falta para que termine el programa.
+        */
+        public void getImagesRGB() 
+        {
+            string path = filePath + @"\BSR_RGB\";
+            System.IO.Directory.CreateDirectory(path);
+            int r = 0; int g = 0;  int b = 0;
+            int number = 1;
+            pathImages = Directory.GetFiles(@"...\BSR\BSDS500\data\images\train", "*.jpg");
+            for (int i = 0; i < pathImages.Length; i++) {
+                image = new Bitmap((Bitmap) Image.FromFile(pathImages[i], true));
+                copiaImagen = new Bitmap(image);
+                for (int x = 0; x < copiaImagen.Width; x++)
+                {
+                    for (int y = 0; y < copiaImagen.Height; y++)
+                    {
+                        pixel = copiaImagen.GetPixel(x, y);
+                        if ((pixel.R > minR || pixel.R < maxR) && (pixel.G > minG || pixel.G < maxG) &&
+                        (pixel.B > minB || pixel.B < maxB))
+                        {
+                            r = 255;
+                            g = 255;
+                            b = 255;
+                        }
+                        else {
+                            r = pixel.R;
+                            g = pixel.G;
+                            b = pixel.B;
+                        }
+                        copiaImagen.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    }
+                }
+                copiaImagen.Save(path + number.ToString() + ".jpg");
+                LBL_IMAGERGB.Text = number.ToString();
+                LBL_IMAGERGB.Refresh();
+                number++;
+            }
+        }
+
+        /**getImagesYUV
+        * Primero se crea una carpeta donde se guardarán las imágenes modificadas.
+        * Se obtendrá de cada imagen su YUV. Si la Y es mayor a .6 (rango de verde), 
+         * se pintará de blanco el pixel. Si no está dentro del rango, se quedará con su color
+        * original.
+        * Cuando se termine de modificar cada imagen, se sumará el parámetro number, el cual es 
+        * utilizado en la interfaz para indicar al usuario cuánto falta para que termine el programa.
+       */
+        public void getImagesYUV()
+        {
+            string path = filePath + @"\BSR_YUV\";
+            System.IO.Directory.CreateDirectory(path);
+            int r = 0; int g = 0; int b = 0;
+            double Y = 0; double U = 0; double V = 0;
+            int number = 1;
+            pathImages = Directory.GetFiles(@"...\BSR\BSDS500\data\images\train", "*.jpg");
+            for (int i = 0; i < pathImages.Length; i++)
+            {
+                image = new Bitmap((Bitmap)Image.FromFile(pathImages[i], true));
+                copiaImagen = new Bitmap(image);
+                for (int x = 0; x < copiaImagen.Width; x++)
+                {
+                    for (int y = 0; y < copiaImagen.Height; y++)
+                    {
+                        pixel = copiaImagen.GetPixel(x, y);
+                        Y = Math.Abs(((.299 * pixel.R) + (.587 * pixel.G) + (.114 * pixel.B))/255);
+                        U = Math.Abs((-(.147 * pixel.R) - (.289 * pixel.G) + (.436 * pixel.G))/255);
+                        V = Math.Abs(((.615 * pixel.R) - (.515 * pixel.G) - (.100 * pixel.B))/255);
+                        if (Y >= .6)
+                        {
+                            r = 255;
+                            g = 255;
+                            b = 255;
+                        }
+                        else
+                        {
+                            r = pixel.R;
+                            g = pixel.G;
+                            b = pixel.B;
+                        }
+                        copiaImagen.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    }
+                }
+                copiaImagen.Save(path + number.ToString() + ".jpg");
+                LBL_IMAGEYUV.Text = number.ToString();
+                LBL_IMAGEYUV.Refresh();
+                number++;
+            }
+        }
+
+        /**getImagesHSV
+        * Primero se crea una carpeta donde se guardarán las imágenes modificadas.
+        * Se obtendrá de cada imagen su HSV. Si éste está dentro del rango obtenido en el método
+        * getHSV, se pintará de blanco el pixel. Si no está dentro del rango, se quedará con su color
+        * original.
+        * Cuando se termine de modificar cada imagen, se sumará el parámetro number, el cual es 
+        * utilizado en la interfaz para indicar al usuario cuánto falta para que termine el programa.
+       **/
+        public void getImagesHSV()
+        {
+            string path = filePath + @"\BSR_HSV\";
+            System.IO.Directory.CreateDirectory(path);
+
+            double rNorm ; 
+            double gNorm = 0; 
+            double bNorm = 0;
+
+            double max = 0; double min = 0; double chroma = 0;
+            int r = 0; int g = 0; int b = 0;
+            double H = 0; double S = 0; double V = 0;
+            int number = 1;
+
+            pathImages = Directory.GetFiles(@"...\BSR\BSDS500\data\images\train", "*.jpg");
+            
+            for (int i = 0; i < pathImages.Length; i++)
+            {
+                image = new Bitmap((Bitmap)Image.FromFile(pathImages[i], true));
+                copiaImagen = new Bitmap(image);
+
+                for (int x = 0; x < copiaImagen.Width; x++)
+                {
+                    for (int y = 0; y < copiaImagen.Height; y++)
+                    {
+                        pixel = copiaImagen.GetPixel(x, y);
+                        rNorm = (double)pixel.R/(double)255;
+                        gNorm = (double)pixel.G/(double)255;
+                        bNorm = (double)pixel.B/(double)255;
+
+                        max = Math.Max(rNorm, Math.Max(gNorm, bNorm));
+                        min = Math.Min(rNorm, Math.Min(gNorm, bNorm));
+                        chroma = max - min;
+
+                        if (max == rNorm)
+                        {
+                            H = 60 * (((gNorm - bNorm) / chroma) % 6);
+                        }
+                        else if (max == gNorm)
+                        {
+                            H = 60 * (((bNorm - rNorm) / chroma) +2);
+                        }
+                        else if (max == bNorm)
+                        {
+                            H = 60 * (((rNorm - gNorm) / chroma) + 4);
+                        }
+
+                        if (max == 0) {S = 0;}
+                        if (max != 0) { S = (chroma / max) * 100; }
+
+                        V = max * 100;
+
+                        if (H > minH && H < maxH)
+                        {
+                            if (S > maxS)
+                            {
+                                r = 255;
+                                g = 255;
+                                b = 255;
+                            }
+                            if (S < minS)
+                            {
+                                r = 255;
+                                g = 255;
+                                b = 255;
+                            }
+                        }
+                        else {
+                            r = pixel.R;
+                            g = pixel.G;
+                            b = pixel.B;
+                        }
+                        copiaImagen.SetPixel(x, y, Color.FromArgb(r, g, b));
+                    }
+                }
+                copiaImagen.Save(path + number.ToString() + ".jpg");
+                LBL_IMAGEHSV.Text = number.ToString();
+                LBL_IMAGEHSV.Refresh();
+                number++;
+            }
+        }
+    }
+}
